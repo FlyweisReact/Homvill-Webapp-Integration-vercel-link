1377b520722c32a21b1924a121128e8234a21d74
FlyweisReact <react1@flyweis.technology>
2025-09-16 00:36:07 +0530

    fix:build error

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ["react-app", "react-app/jest"],
-  rules: {
-    "no-unused-vars": "off", // disable unused variable warnings
-    "jsx-a11y/alt-text": "off", // disable alt text warnings
-  },
-};
